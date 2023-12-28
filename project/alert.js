e0a669059e96194fb45368d0fc626fa778d487d2
Zippitydoda <szipperer608@gmail.com>
2023-12-28 16:43:36 -0600

    Email opens 2 window instead of 1

--- project/alert.js
+++ project/alert.js
@@ -1,3 +1,5 @@
+// Sms notification handler
+
 // Select the node that will be observed for mutations
 const targetNode = document.getElementById("smsnotifications");
 
@@ -30,3 +32,33 @@ const observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
 observer.observe(targetNode, config);
+
+//--------------------------------------------------------------------------------
+// E-mail notification handler
+
+// Select the node that will be observed for mutations
+const targetNodeEmail = document.getElementById("emailnotifications");
+console.log(
+    targetNodeEmail.getElementsByClassName("badge badge-notify")[0].innerHTML
+);
+// Options for the observer (which mutations to observe)
+const configEmail = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callbackEmail = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+            window.open(
+                "https://app.omnique.com/Company/427900/Shop/1/Notification/RedirectToWebmail"
+            );
+
+            return;
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observerEmail = new MutationObserver(callbackEmail);
+
+// Start observing the target node for configured mutations
+observerEmail.observe(targetNodeEmail, configEmail);
